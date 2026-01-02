68ff231285fff5a39e30be05841874648e0caac3
cristina chiu-cucu <cris.chiu.cucu@gmail.com>
2026-01-02 12:24:51 +0200

    Replaced BrowserRouter with HashRouter.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 
 import App from "./App.tsx";
 import EpisodeListPage from "./pages/episode-list-page/EpisodeListPage.tsx";
@@ -10,13 +10,13 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<EpisodeListPage />} />
           <Route path="episode/:episodeId" element={<EpisodeDetailsPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
